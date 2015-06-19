5c1a30bccaf164edfa187024313944b7cdb42462
Veasna Uch <veasnauch.vsn@gmail.com>
2015-06-19 08:10:36 +0000

    update

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,26 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/create', function(req, res, next) {
+  var vm = {
+    title: 'Create an account'
+  };
+  res.render('users/create', vm);
+})
+
+router.post('/create', function(req, res, next) {
+  var sthWrong = false;
+  if (sthWrong) {
+    var vm = {
+      title: 'Create an account',
+      input: req.body,
+      error: 'Something went wrong'
+    };
+    delete vm.input.password;
+    return res.render('users/create', vm);
+  }
+  delete vm.input.password;
+  res.render('/orders');
+})
+
 module.exports = router;
